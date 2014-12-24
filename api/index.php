530b96977b16fbbd5b3bb5934a2b1576b404f7fc
Jenkins Continuous Build server <jenkins@dev.local>
2014-12-24 13:55:20 +0300

    first

---
+++ api/index.php
@@ -0,0 +1,39 @@
+<?php
+
+//print_r($_REQUEST);
+//print_r($_FILES);
+
+$u = uniqid();
+$source = $_FILES['file']['tmp_name'];
+$e = getExtension(getFileMimeType($source));
+$destination = __DIR__ . '/../files/' . $u . "." . $e;
+
+move_uploaded_file($source, $destination);
+
+echo $u . "." . $e . "\n";
+
+// -------------------------------------------------
+
+function getFileMimeType($file) {
+    if (function_exists('finfo_file')) {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $type = finfo_file($finfo, $file);
+        finfo_close($finfo);
+    } else {
+        require_once 'upgradephp/ext/mime.php';
+        $type = mime_content_type($file);
+    }
+    return $type;
+}
+
+function getExtension($type) {
+    $a = [
+       'image/png' => 'png',
+       'image/jpg' => 'jpg'
+    ];
+    if (array_key_exists($type, $a)) {
+        return $a[$type];
+    } else {
+        return 'unknown';
+    }
+}
